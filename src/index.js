121dd7eb9f01b33ed7144b7ec01091c002739829
po111 <98609236+po111@users.noreply.github.com>
2025-06-06 19:43:52 +0300

    Add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('It still works!!??');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Working of course!")
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
