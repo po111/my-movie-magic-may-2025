a479ca60fca832e0ed2b22d9e5e31f016f4b40b4
po111 <98609236+po111@users.noreply.github.com>
2025-06-10 17:11:51 +0300

    Added movie service

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import movieService from '../services/movieService.js';
 
 const homeController =  express.Router();
 
 //Config routes
 homeController.get('/', (req, res) => {
+const movies = movieService.getAll();
+console.log(movies)
+
     res.render('home');
 });
 
