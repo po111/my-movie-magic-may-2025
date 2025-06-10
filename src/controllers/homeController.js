325bff46f6650b431d21b3ad31e162427e721099
po111 <98609236+po111@users.noreply.github.com>
2025-06-10 17:59:20 +0300

    Render dynamic movies/no movies home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,7 +8,7 @@ homeController.get('/', (req, res) => {
 const movies = movieService.getAll();
 console.log(movies)
 
-    res.render('home');
+    res.render('home', { movies });
 });
 
 homeController.get('/about', (req, res) => {
