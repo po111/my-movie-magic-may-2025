d12e19147c0d9f69c97cff394aa1a68bbd0699a6
po111 <98609236+po111@users.noreply.github.com>
2025-06-12 12:25:00 +0300

    Added movie model and persistance

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -8,12 +8,12 @@ movieController.get('/create', (req,res) => {
     res.render('create');
 });
 
-movieController.post('/create', (req, res) => {
+movieController.post('/create', async (req, res) => {
 
     const newMovie = req.body;
 
     //Save movie
-    movieService.create(newMovie);
+    await movieService.create(newMovie);
 
     //Redirect to home page
     res.redirect('/');
