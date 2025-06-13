9334346110d159a58fa72bfa7cf5e1feb857330d
po111 <98609236+po111@users.noreply.github.com>
2025-06-13 23:04:27 +0300

    Created mongoose model Movie

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,27 +1,46 @@
-import fs from 'node:fs/promises';
-import{v4 as uuid} from 'uuid';
+import {Schema, model} from 'mongoose';
 
-const moviesJson = await fs.readFile('./src/database.json');
-export const movies = JSON.parse(moviesJson);
+const movieSchema =  new Schema({
+    title: String,
+    category: String,
+    genre: stringify,
+    director: stringify,
+    year: Number,
+    imageURL: String,
+    rating: Number,
+    description: String
+});
+
+const Movie = model('Movie', movieSchema);
+
+export default Movie;
+
+//Old - for working with file instead of DB
+
+// import fs from 'node:fs/promises';
+// import{v4 as uuid} from 'uuid';
+
+// const moviesJson = await fs.readFile('./src/database.json');
+// export const movies = JSON.parse(moviesJson);
 
 
-export default class Movie {
-    constructor(data) {
-        this.data =  data;
-    }
+// export default class Movie {
+//     constructor(data) {
+//         this.data =  data;
+//     }
     
-    async save() {
+//     async save() {
                 
-        //Set unique id
-        this.data.id= uuid();
+//         //Set unique id
+//         this.data.id= uuid();
 
-        // //Convert rating from string to number
-        this.data.rating = Number(this.data.rating);
+//         // //Convert rating from string to number
+//         this.data.rating = Number(this.data.rating);
 
-        movies.push(this.data);
+//         movies.push(this.data);
 
-        await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
+//         await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
 
-        return this.data;
-    }
-}
+//         return this.data;
+//     }
+// }
