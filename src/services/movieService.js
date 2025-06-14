61efb1c0d777b94961a407ecd67b60e5daa299e6
po111 <98609236+po111@users.noreply.github.com>
2025-06-14 15:10:45 +0300

    Modify cast create hbs

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,7 +6,7 @@ export default {
      async getAll(filter = {}) {
 
         let result = await Movie.find({}).lean();
-        console.log(result);
+        //console.log(result);
 
         if(filter.search) {
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));           
