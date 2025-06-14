68ddc855585bb9fd848219cad4beba66f46b3266
po111 <98609236+po111@users.noreply.github.com>
2025-06-14 17:34:13 +0300

    Added attach page with dynamic movie data

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -40,6 +40,14 @@ movieController.get('/search', async (req, res) => {
     const movies = await movieService.getAll(filter);
     res.render('search', {movies, filter});
 
+});
+
+movieController.get('/:movieId/attach', async (req, res) => {
+  const movieId = req.params.movieId;
+
+  const movie = await movieService.getOne(movieId);
+  
+  res.render('movie/attach', { movie} );
 })
 
 
